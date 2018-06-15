7717ed7514e76b7501635ab703919f5d0be4bc9b
Shiva <shiva.srajah@gmail.com>
2018-06-15 19:10:05 +0200

    Front test OK Waiting for Rizvane :D

--- front-end/src/app.js
+++ front-end/src/app.js
@@ -1,17 +1,3 @@
 import {messages} from "./messages";
 import axios from 'axios'
 
-const webSocket = new WebSocket('ws://localhost:8888');
-
-webSocket.onmessage = (e) => {
-    let response = JSON.parse(e.data);
-    if(response !== undefined){
-        axios.get('http://localhost:8080')
-            .then(function (httpResponse) {
-                console.log(httpResponse.data);
-            })
-    }else{
-        document.getElementById('container').innerHTML = "<h1>Erreur lors de la connexion au Back-End</h1>"
-    }
-
-}
